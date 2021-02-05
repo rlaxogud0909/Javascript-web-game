53fac6289c22086c18da1a0e1f4c53f37bb282f7
rlaxogud0909 <63087652+rlaxogud0909@users.noreply.github.com>
2021-02-05 20:50:01 +0900

    Update 1.js

--- 끝말잇기/1.js
+++ 끝말잇기/1.js
@@ -2,11 +2,14 @@ var word = '시작';
 
 while(true) { 
     var answer = prompt(word);
-    if (answer === '포기') { break; }
+    if (answer === '포기') { break; } // 게임 
+    //word의 끝 단어와 answer의 첫단어가 동일하면
     if (word[word.length - 1] === answer[0]) {
+        // 정답
         alert('yes');
         word = answer;
     } else {
+        //오답
         alert('no');
     }
 }
