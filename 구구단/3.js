62e687d3cf6b3776887b3eed55067086c77790ee
rlaxogud0909 <63087652+rlaxogud0909@users.noreply.github.com>
2021-02-06 21:54:23 +0900

    Update 3.js

--- 구구단/3.js
+++ 구구단/3.js
@@ -1,6 +1,6 @@
 while (true) {
-  var 숫자1 = Math.floor(Math.random() * 9) + 1
-  var 숫자2 = Math.floor(Math.random() * 9) + 1
+  var 숫자1 = Math.ceil(Math.random() * 9) + 1
+  var 숫자2 = Math.ceil(Math.random() * 9) + 1
   var 결과 = 숫자1 * 숫자2
   var 조건 = true;
   while (조건) {
